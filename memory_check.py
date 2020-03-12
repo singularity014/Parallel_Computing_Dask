f752775c3e9f07ec0a12167ae8fd6246b993eac3
psharma92 <psharma@naister.com>
2020-03-12 23:37:41 +0100

    adding info.

--- memory_check.py
+++ memory_check.py
@@ -3,6 +3,9 @@ import os
 
 # Querying Python interpreter's memory usage
 # Function to be called before and after every operation
+# Tells how much memory a python operation is using
+# This operation could be as simple as sum/mul to as complex as Embedding Calculation etc.
+
 def memory_footprint():
     """
     :return: memory in MB being used by python process
